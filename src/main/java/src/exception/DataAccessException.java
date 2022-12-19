fa719d773043f94f483f864cf432372a4d39abc0
wzq20021202 <3033715900@qq.com>
2022-12-19 14:24:29 +0800

    exception

---
+++ src/main/java/src/exception/DataAccessException.java
@@ -0,0 +1,38 @@
+package src.exception;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author 30337
+ */
+@Service
+public class DataAccessException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DataAccessException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public DataAccessException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DataAccessException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DataAccessException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
